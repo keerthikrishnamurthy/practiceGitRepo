0b7084033af1f10ec62ebef33d6f723aa8806a34
keerthikrish <keerthikrish2019@gmail.com>
2018-10-12 22:32:48 -0700

    PracticeGit.java added

---
+++ PracticeGit.java
@@ -0,0 +1,8 @@
+public class PracticeGit {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hi");
+        System.out.println("1");
+        
+    }
+}
